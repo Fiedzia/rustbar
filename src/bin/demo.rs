6c79432792bddb6a484e0151815109e20a86ddf0
Maciej Dziardziel <fiedzia@gmail.com>
2015-06-19 00:40:23 +0100

    add InfiniteProgressBar

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -7,10 +7,20 @@ use rustbar::rustbars::ProgressBar;
 pub fn main(){
 
     let mut pbar = rustbar::rustbars::PercentageProgressBar::new();
-    println!("PercentageProgressBar demo");
+    println!("\nPercentageProgressBar demo");
     for idx in 0..101 {
         pbar.set_value(idx);
-        pbar.show();
+        pbar.set_msg("Calculating...");
+        pbar.render();
         sleep_ms(50);
     }
+
+    println!("\nInifiniteProgressBar demo");
+    let mut infbar  = rustbar::rustbars::InfiniteProgressBar::new();
+    for _ in 0..101 {
+        infbar.set_msg("Thinking...");
+        infbar.render();
+        sleep_ms(50);
+    }
+
 }
--- src/rustbars.rs
+++ src/rustbars.rs
@@ -8,6 +8,7 @@ pub trait ProgressBar<T> {
 #[derive(Default)]
 pub struct PercentageProgressBar {
     value: u8, //0..100
+    msg:   String
 }
 
 impl ProgressBar<PercentageProgressBar> for PercentageProgressBar {
@@ -19,19 +20,66 @@ impl ProgressBar<PercentageProgressBar> for PercentageProgressBar {
 
 impl PercentageProgressBar {
 
-    pub fn show(&self) {
-        print!("\r{}%", self.value);
-        stdout().flush().ok().expect("");
+    pub fn render(&self) {
+        print!("\r{msg}{value}%", msg=self.msg, value=self.value);
+        stdout().flush().ok().expect("write to stdout failed");
     }
 
     pub fn set_value(&mut self, value: u8) { if value <= 100 { self.value = value } }
     pub fn get_value(&self) -> u8 { self.value }
+
+    pub fn set_msg(&mut self, msg: &str) { self.msg = msg.to_owned() }
+    pub fn get_msg(&self) -> &str { self.msg.as_ref() }
+
+
     pub fn inc(&mut self) { if self.value < 100 { self.value += 1; } }
     pub fn dec(&mut self) { if self.value > 0 { self.value -= 1; } }
 
 }
 
-#[test]
-fn test_percentage_progressbar() {
-    let mut ppb = PercentageProgressBar { ..Default::default()};
+pub struct InfiniteProgressBar {
+    msg:   String,
+    marker_position:  i8,
+    step: i8
+}
+
+impl Default for InfiniteProgressBar {
+    fn default() -> InfiniteProgressBar {
+        InfiniteProgressBar {
+            step: 1,
+            msg: "".to_owned(),
+            marker_position: 0
+        }
+    }
+}
+
+impl ProgressBar<InfiniteProgressBar> for InfiniteProgressBar {
+
+    fn new() -> InfiniteProgressBar {
+        InfiniteProgressBar { ..Default::default()}
+    }
+}
+
+impl InfiniteProgressBar {
+
+    pub fn set_msg(&mut self, msg: &str) { self.msg = msg.to_owned() }
+    pub fn get_msg(&self) -> &str { self.msg.as_ref() }
+
+    pub fn render(&mut self) {
+        if self.marker_position <= 0 {
+            self.marker_position = 0;
+            self.step = 1;
+        } else if self.marker_position > 9 {
+            self.marker_position = 10;
+            self.step = -1;
+        }
+        self.marker_position = self.marker_position + self.step;
+        
+
+        let mut bar:String = "..........".to_owned(); //10 dots
+        bar.insert(self.marker_position as usize, '#');
+        print!("\r{msg}[{bar}]", msg=self.msg, bar=bar);
+        stdout().flush().ok().expect("write to stdout failed");
+    }
+   
 }
